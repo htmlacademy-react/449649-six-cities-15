019b33a4955020b4a8777574bf764ddef2673811
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-02-21 10:09:35 +0100

    Добавил маршрутизацию и страницу 404

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute } from '../../const';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
 
 type AppPageProps = {
   placesCount: number;
@@ -6,7 +12,15 @@ type AppPageProps = {
 
 function App({ placesCount }: AppPageProps): JSX.Element {
   return (
-    <MainPage placesCount={placesCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Main} element={<MainPage placesCount={placesCount} />} />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path={AppRoute.Offer} element={<OfferPage />} />
+        <Route path={AppRoute.Favorites} element={<FavoritesPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/const.tsx
+++ src/const.tsx
@@ -1,3 +1,12 @@
-export const Setting = {
+const Setting = {
   PlacesCount: 312
 };
+
+enum AppRoute {
+  Main = '/',
+  Login = '/login',
+  Favorites = '/favorites',
+  Offer = '/offer/:id',
+}
+
+export { Setting, AppRoute };
