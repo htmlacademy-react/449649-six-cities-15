53530e0d33d9df5950dd6f1a9dd64a671c645628
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-02-24 14:19:01 +0100

    Добавил моки

---
+++ src/types/offer.ts
@@ -0,0 +1,23 @@
+import { City } from './city';
+import { Location } from './location';
+import { Host } from './host';
+
+export type Offer = {
+  id: string;
+  title: string;
+  type: string;
+  price: number;
+  rating: number;
+  bedrooms: number;
+  maxAdults: number;
+  isPremium: boolean;
+  isFavorite: boolean;
+  description: string;
+  images: string[];
+  location: Location;
+  city: City;
+  host: Host;
+  goods: string[];
+};
+
+export type Offers = Offer[];
