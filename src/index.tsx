5f67310e5992686a1002663928b7354c1a725876
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-03-19 19:53:36 +0100

    Добавил обработку ошибок взаимодействия с сервером

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import { REVIEWS } from './mocks/reviews';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import ErrorMessage from './components/error-message/error-message';
+import { checkAuthAction } from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
