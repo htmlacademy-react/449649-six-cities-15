8a6caf85f156c3344f7cd92a06587a49f4746bf9
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-03-21 15:47:02 +0100

    Добавил middleware и новое действие

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
 import { City, Offers } from '../types/types';
-import { AuthorizationStatus } from '../const';
+import { AppRoute, AuthorizationStatus } from '../const';
 
 export const setCity = createAction('SET_CITY', (city: City) => ({ payload: city, }));
 export const setOffers = createAction('SET_OFFERS', (offers: Offers) => ({ payload: offers, }));
@@ -9,3 +9,4 @@ export const setError = createAction<string | null>('SET_ERROR');
 export const loadOffers = createAction('LOAD_OFFERS', (offers: Offers) => ({ payload: offers }));
 export const requireAuthorization = createAction('REQUIRE_AUTHORIZATION', (status: AuthorizationStatus) => ({ payload: status }));
 export const setOffersDataLoadingStatus = createAction<boolean>('SET_OFFERS_DATA_LOADING_STATUS');
+export const redirectToRoute = createAction<AppRoute>('REDIRECT_TO_ROUTE');
