ed995e134b63f97cfba8e0ff9fb9b5bb864f9ab5
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-03-27 16:19:13 +0100

    Добавил редирект на страницу MainEmptyPage при отсутствии offers

--- src/pages/main-empty-page/main-empty-page.tsx
+++ src/pages/main-empty-page/main-empty-page.tsx
@@ -1,30 +1,22 @@
-import Header from '../../components/header/header';
-import CitiesTabs from '../../components/cities-tabs/cities-tabs';
 import { useAppSelector } from '../../hooks/useApp';
-import { getCity } from '../../store/offers-data/selectors';
+import { getCityName } from '../../store/offers-data/selectors';
 
 function MainEmptyPage(): JSX.Element {
-  const city = useAppSelector(getCity);
+  const cityName = useAppSelector(getCityName);
 
   return (
-    <div className="page page--gray page--main">
-      <Header />
-      <main className="page__main page__main--index page__main--index-empty">
-        <CitiesTabs />
-        <div className="cities">
-          <div className="cities__places-container cities__places-container--empty container">
-            <section className="cities__no-places">
-              <div className="cities__status-wrapper tabs__content">
-                <b className="cities__status">No places to stay available</b>
-                <p className="cities__status-description">
-                  We could not find any property available at the moment in {city?.name}
-                </p>
-              </div>
-            </section>
-            <div className="cities__right-section" />
+    <div className="cities">
+      <div className="cities__places-container cities__places-container--empty container">
+        <section className="cities__no-places">
+          <div className="cities__status-wrapper tabs__content">
+            <b className="cities__status">No places to stay available</b>
+            <p className="cities__status-description">
+              We could not find any property available at the moment in {cityName}
+            </p>
           </div>
-        </div>
-      </main>
+        </section>
+        <div className="cities__right-section" />
+      </div>
     </div>
   );
 }
