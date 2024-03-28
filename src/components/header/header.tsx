2cdbaafb0c7b7143f97fd7962cfec57753ec0825
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-03-28 10:24:23 +0100

    Добавил отображение кол-ва favorite offers в header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,10 +4,12 @@ import { useAppSelector } from '../../hooks/useApp';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import { Link, NavLink } from 'react-router-dom';
 import { getAuthorizationStatus, getUser } from '../../store/user-data/selectors';
+import { getFavoriteOffers } from '../../store/favorite-offers-data/selectors';
 
 function Header(): JSX.Element {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const user = useAppSelector(getUser);
+  const favoriteOffers = useAppSelector(getFavoriteOffers);
   const isLoggedIn = authorizationStatus === AuthorizationStatus.Auth;
 
   return (
@@ -28,7 +30,7 @@ function Header(): JSX.Element {
                     >
                     </div>
                     <span className="header__user-name user__name">{user?.name}</span>
-                    <span className="header__favorite-count">3</span>
+                    <span className="header__favorite-count">{favoriteOffers.length}</span>
                   </NavLink>
                 </li>
                 <li className="header__nav-item">
