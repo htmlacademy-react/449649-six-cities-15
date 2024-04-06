5b77ead733c5f440c4b6b24d8f692019b58f0923
Dmitriy Yurkin <dmitrii.iurkin@yandex.ru>
2024-04-06 12:42:30 +0200

    Исправил замечания по коду

--- src/store/user-data/selectors.ts
+++ src/store/user-data/selectors.ts
@@ -2,5 +2,4 @@ import { AuthorizationStatus, NameSpace } from '../../const';
 import { State, UserData } from '../../types/types';
 
 export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
-export const getAuthCheckedStatus = (state: State): boolean => state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
 export const getUser = (state: State): UserData | null => state[NameSpace.User].user;
